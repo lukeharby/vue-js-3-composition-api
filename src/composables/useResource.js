b64298946f9de5e393e11ecb9c3e758a164027e3
Luke Harby <luke@amiqus.co>
2025-01-20 10:07:33 +0000

    Add pageResource composable

--- src/composables/useResource.js
+++ src/composables/useResource.js
@@ -1,19 +1,15 @@
 import { ref } from 'vue';
 
+import usePageRequests from './usePageRequests';
+
 export default function useResource(resource) {
+    const { makeRequest } = usePageRequests();
     const items = ref([]);
     const item = ref(null);
     const baseURL = `https://jsonplaceholder.typicode.com/${resource}`;
 
-    const fetchItems = async () => {
-        const response = await fetch(baseURL);
-        items.value = await response.json();
-    }
-
-    const fetchItem = async (id) => {
-        const response = await fetch(`${baseURL}/${id}`);
-        item.value = await response.json();
-    }
+    const fetchItems = async () => items.value = await makeRequest(baseURL);
+    const fetchItem = async (id) => item.value = await makeRequest(`${baseURL}/${id}`);
 
     return {
         items,
